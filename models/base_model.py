948a0f4d0bff57514bb49ce8847a8b76926325e4
root <brendajeptum@gmail.com>
2023-05-11 11:36:11 -0700

    Use of *kwargs as constructor of the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,30 @@ import models
 
 class BaseModel:
     """ Definiton of all common attributes/methods for other classes"""
+    """
     def __init__(self):
-        """Definition of common attributes/methods for other classes"""
+        Instantiation of classs
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+     """
+
+    def __init__(self, *args, **kwargs):
+        """Instantiation of class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    setattr(self, key, eval(value))
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value,
+                                                       '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Prints out instance created as a string"""
