20736ba31cd7b27d6567265abe536f0629a7fadd
root <brendajeptum@gmail.com>
2023-05-15 19:48:40 -0700

    Updated BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -33,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Prints out instance created as a string"""
@@ -42,6 +43,8 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        # Link BaseModel to FileStorage
+        models.storage.save()
 
     def to_dict(self):
         """Returns dictionary with all keys/values of __dict__ of instance"""
